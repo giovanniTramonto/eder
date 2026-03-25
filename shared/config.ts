6b1a7d1ec17e6c8ee5e05d59f5f3c007389b3750
Johannes Abendroth <johannes.abendroth@gmail.com>
2026-03-25 14:51:49 +0100

    feat: change config

--- shared/config.ts
+++ shared/config.ts
@@ -1,3 +1,3 @@
-export const MAX_PROMPT_LENGTH = 50
-export const MAX_SESSION_REQUESTS = 5
+export const MAX_PROMPT_LENGTH = 80
+export const MAX_SESSION_REQUESTS = 10
 export const SESSION_DURATION_MS = 60 * 60 * 1000
